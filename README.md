bad044fab53847b8235621d2ce8ed489d4f3096f
a-madhukar <a.madhukar@yahoo.com>
2019-11-20 19:39:05 +0800

    add the array & the readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# PROBLEM 
+
+- Calculate the total price of all products where the keys *ARE NOT* blank, empty, undefined, NaN, etc. 
+
+- keep in mind that key names in the object are likely to change. E.g., The item key can renamed to item_key or title. But your solution should still work. 
+
+- Console log the total value of the products. Your total value should be 400. 
---
+++ app.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    name: 'item 1', 
+    price:10000, 
+    category: 'category 1'
+  },
+  
+  {
+    name:'', 
+    price:10000, 
+    category: 'category 2'
+  },
+  
+  {
+    name:'item 3', 
+    price:10000, 
+    category: 'category 3'
+  },
+  
+  {
+    name:'item 4', 
+    price:10000, 
+    category: 'category 4'
+  },
+  
+  {
+    name:'item 5', 
+    price:0, 
+    category: 'category 5'
+  },
+  
+  {
+    name:'item 6', 
+    price:10000, 
+    category: null
+  },
+  
+    {
+    name:'item 7', 
+    price:10000, 
+    category: 'category 7'
+  }
+]; 
+
+
+// Your Solution goes here. 
+
+
